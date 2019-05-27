958364b36df913bf71ac2d0cdbfb0d5c6366dfad
marco <48846555+mlrco@users.noreply.github.com>
2019-05-27 07:44:55 -0600

    Add files via upload

---
+++ getPHP.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE HTML>
+<html lang=es> 
+<head>
+    <title>Metodo GET - PHP</title>
+</head> 
+<body>
+
+    <?php
+
+    $nombre = $_GET['nombre'];
+
+    echo "Hola, ".$nombre;
+//¿por qué no puedo correr los archivos php localmente? me parece una injusticia
+
+    ?>
+
+</body>
+</html>
